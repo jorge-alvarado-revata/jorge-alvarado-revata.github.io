4e35b8fc55a7c2267c7cef707ff8eb86cd43b65f
died_sistemas12 <died_sistemas12@minedu.gob.pe>
2024-01-07 00:59:56 -0500

    add card

--- portfolio/src/app/face/face.component.ts
+++ portfolio/src/app/face/face.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../module/material/material.module';
+
+import Chart from 'chart.js';
 
 @Component({
   selector: 'face',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './face.component.html',
   styleUrl: './face.component.scss'
 })
--- portfolio/src/app/module/material/material.module.ts
+++ portfolio/src/app/module/material/material.module.ts
@@ -5,8 +5,9 @@ import {MatListModule} from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
 
-const components=[MatSidenavModule,  MatListModule, MatIconModule, MatToolbarModule, MatButtonModule]
+const components=[MatSidenavModule,  MatListModule, MatIconModule, MatToolbarModule, MatButtonModule, MatCardModule]
 
 @NgModule({
   declarations: [],

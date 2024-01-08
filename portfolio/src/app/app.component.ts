f123466b0940122b7a99a6e71b9c7ff7df98801c
died_sistemas12 <died_sistemas12@minedu.gob.pe>
2024-01-08 00:09:49 -0500

    add chart bar

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,21 +1,16 @@
 import { Component } from '@angular/core';
 import { MainComponent } from './main/main.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSidenavModule} from '@angular/material/sidenav';
 import { MenuServiceService } from './menu-service.service';
 
+import { MaterialModule } from './module/material/material.module';
 
-const LIST_COMPONENTS = [MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule, MatMenuModule, MatSidenavModule, MainComponent];
+const LIST_COMPONENTS = [MainComponent];
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: LIST_COMPONENTS,
+  imports: [LIST_COMPONENTS],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- portfolio/src/app/face/face.component.ts
+++ portfolio/src/app/face/face.component.ts
@@ -1,15 +1,62 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MaterialModule } from '../module/material/material.module';
+import { NgChartsModule } from 'ng2-charts';
 
-import Chart from 'chart.js';
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'face',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, NgChartsModule],
   templateUrl: './face.component.html',
   styleUrl: './face.component.scss'
 })
-export class FaceComponent {
+export class FaceComponent implements OnInit, AfterViewInit {
+
+
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10,
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      },
+    },
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartPlugins = [DataLabelsPlugin];
+
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2003-2008', '2008-2012', '2012-2013', '2014-2016', '2021-2022', '2022-2023', '2023', '2023-today'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40, 40], label: 'Front End' },
+      { data: [28, 48, 40, 19, 86, 27, 90, 40], label: 'Back End' },
+      { data: [65, 70, 20, 10, 86, 27, 90, 40], label: 'FullStack' },
+    ],
+  };
+
+  constructor(){}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+
+
+  }
+
 
 }
